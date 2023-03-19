32d87b47075c1dd80c72064965766247b79b8f79
vasyl <vasilijpodlevskij@gmail.com>
2023-03-19 14:33:19 +0100

    zadanie 1

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const div = document.createElement('h2')
+const body = document.querySelector('body')
+body.appendChild(div);
+div.textContent = 'Hello world JS'
+
+// zadanie Teoria JS 16
+
+// zadanie 1
+const myVariable = 10;
+const myName = "Benedykt";
+
+console.log(typeof myVariable);
+console.log(typeof myName);
